1c48a15031a520fa87d32dcf6b956a38e0cfc2f8
Tobias Krojer <tobkro@w-tobkro-pc-0.maxiv.lu.se>
2022-02-22 11:33:14 +0100

    first commit

--- convert_event_map_to_mtz.py
+++ convert_event_map_to_mtz.py
@@ -5,8 +5,23 @@ import os
 import gemmi
 
 
-def change_axis_order():
-    print('hallo')
+def change_axis_order(map_name, axisOrder):
+    if len(axisOrder) != 3:
+        print('ERROR: axis order needs to contain three characters of type X, Y and Z')
+        sys.exit(2)
+
+    a = upper(axisOrder[0])
+    b = upper(axisOrder[1])
+    c = upper(axisOrder[2])
+
+    cmd = (
+        'mapmask mapin {0!s} mapout tmp.map << eof\n'.format(map_name) +
+        ' axis {0!s} {1!s} {2!s}\n'.format(a, b, c)
+        ' end\n'
+        'eof'
+    )
+
+    print(cmd)
 
 def convert_event_maps_to_mtz(panddaDir, axisOrder):
     print('looking for event maps in {0!s}'.format(panddaDir))
@@ -16,7 +31,7 @@ def convert_event_maps_to_mtz(panddaDir, axisOrder):
         map_name = maps.split('/')[len(maps.split('/'))-1]
         print(map_name)
         if axisOrder:
-            change_axis_order()
+            change_axis_order(map_name, axisOrder)
 
 def main(argv):
     panddaDir = None
