650502ee149c55e31af39c71b2321fb2ba10f004
Tobias Krojer <tobkro@w-tobkro-pc-0.maxiv.lu.se>
2022-02-22 11:23:33 +0100

    first commit

--- convert_event_map_to_mtz.py
+++ convert_event_map_to_mtz.py
@@ -5,32 +5,40 @@ import os
 import gemmi
 
 
-def convert_event_maps_to_mtz(panddaDir):
+def change_axis_order():
+    print('hallo')
+
+def convert_event_maps_to_mtz(panddaDir, axisOrder):
     print('looking for event maps in {0!s}'.format(panddaDir))
     for maps in sorted(glob.glob(os.path.join(panddaDir, 'processed_datasets', '*', '*.ccp4'))):
         sample_id = maps.split('/')[len(maps.split('/'))-2]
         os.chdir(maps[:maps.rfind('/')])
         map_name = maps.split('/')[len(maps.split('/'))-1]
         print(map_name)
+        if axisOrder:
+            change_axis_order()
 
 def main(argv):
     panddaDir = None
+    axisOrder = None
 
     try:
-        opts, args = getopt.getopt(argv,"hp:",["pdir="])
+        opts, args = getopt.getopt(argv,"hp:a:",["panddadir=", "axis="])
     except getopt.GetoptError:
-        print('ccp4-python convert_event_map_to_mtz.py -p <pandda_dir>')
+        print('ccp4-python convert_event_map_to_mtz.py -p <pandda_dir> -a <axis>')
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
             print('ccp4-python convert_event_map_to_mtz.py -p <pandda_dir>')
             sys.exit()
-        elif opt in ("-p", "--pdir"):
+        elif opt in ("-p", "--panddadir"):
+            panddaDir = arg
+        elif opt in ("-a", "--axis"):
             panddaDir = arg
 
     if os.path.isdir(panddaDir):
-        convert_event_maps_to_mtz(panddaDir)
+        convert_event_maps_to_mtz(panddaDir, axisOrder)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
