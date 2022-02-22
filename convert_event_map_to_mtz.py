485692d579fb00c7a04dcadbfff78b3aee6580aa
Tobias Krojer <tobkro@w-tobkro-pc-0.maxiv.lu.se>
2022-02-22 11:39:29 +0100

    first commit

--- convert_event_map_to_mtz.py
+++ convert_event_map_to_mtz.py
@@ -23,7 +23,11 @@ def change_axis_order(map_name, axisOrder):
 
     print(cmd)
 
-def convert_event_maps_to_mtz(panddaDir, axisOrder):
+def convert_event_maps_to_mtz(panddaDir, axisOrder, overwrite):
+    if overwrite:
+        print('ALARM')
+        sys.exit(2)
+
     print('looking for event maps in {0!s}'.format(panddaDir))
     for maps in sorted(glob.glob(os.path.join(panddaDir, 'processed_datasets', '*', '*.ccp4'))):
         sample_id = maps.split('/')[len(maps.split('/'))-2]
@@ -36,9 +40,10 @@ def convert_event_maps_to_mtz(panddaDir, axisOrder):
 def main(argv):
     panddaDir = None
     axisOrder = None
+    overwrite = False
 
     try:
-        opts, args = getopt.getopt(argv,"hp:a:",["panddadir=", "axis="])
+        opts, args = getopt.getopt(argv,"hp:a:o:",["panddadir=", "axis=", "overwrite"])
     except getopt.GetoptError:
         print('ccp4-python convert_event_map_to_mtz.py -p <pandda_dir> -a <axis>')
         sys.exit(2)
@@ -50,10 +55,10 @@ def main(argv):
         elif opt in ("-p", "--panddadir"):
             panddaDir = arg
         elif opt in ("-a", "--axis"):
-            axisOrder = arg
+            overwrite = True
 
     if os.path.isdir(panddaDir):
-        convert_event_maps_to_mtz(panddaDir, axisOrder)
+        convert_event_maps_to_mtz(panddaDir, axisOrder, overwrite)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
