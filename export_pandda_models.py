f3789081bab4aa5c40dceb8233b7255ad97aa890
Tobias Krojer <tobias.krojer@maxiv.lu.se>
2022-03-04 10:35:29 +0100

    first commit

--- export_pandda_models.py
+++ export_pandda_models.py
@@ -26,6 +26,7 @@ import csv
 from tabulate import tabulate
 import gemmi
 
+
 def read_inspect_event_csv_as_list(panddaDir):
     inspect_csv = os.path.join(panddaDir, 'analyses', 'pandda_inspect_events.csv')
     if not os.path.isfile(inspect_csv):
@@ -46,18 +47,24 @@ def get_ligand_confidence_index(inspect_csv):
 
 
 def get_info(inspect_csv, sample_id, ligand_confidence_index, model):
+    ligand_confidence_list = []
     table = []
-    header = ['Event', 'Site', 'BDC', 'Ligand ID', 'Ligand confidence']
+    header = ['Event', 'Site', 'BDC', 'Ligand ID', 'Distance', 'Ligand confidence']
     for item in inspect_csv:
         if item[0] == sample_id:
             event = gemmi.Position(float(item[12]), float(item[13]), float(item[13]))
-            ligand = assign_modelled_ligand_to_event_coordinate(model, event)
-            table.append([item[1], item[11], item[2], ligand, item[ligand_confidence_index]])
-    print(tabulate((table), headers=header))
-    print("\n\n")
+            ligand, distance = assign_modelled_ligand_to_event_coordinate(model, event)
+            table.append([item[1], item[11], item[2], ligand, distance, item[ligand_confidence_index]])
+            if item[ligand_confidence_index] not in ligand_confidence_list:
+                ligand_confidence_list.append(item[ligand_confidence_index])
+    print(tabulate(table, headers=header))
+    print("\n")
+    return ligand_confidence_list
+
 
 def assign_modelled_ligand_to_event_coordinate(model, event):
     lig = "unknown"
+    distance = "unknown"
     structure = gemmi.read_structure(model, merge_chain_parts=True)
     for mod in structure:
         for chain in mod:
@@ -65,87 +72,163 @@ def assign_modelled_ligand_to_event_coordinate(model, event):
                 if residue.name == 'LIG':
                     c = gemmi.Chain(chain.name)
                     c.add_residue(residue, 0)
-                    distance = event.dist(c.calculate_center_of_mass())
+                    distance = round(event.dist(c.calculate_center_of_mass()), 2)
 #                    print(distance, event.tolist(), c.calculate_center_of_mass().tolist())
                     if distance < 10:
                         lig = residue.name + '-' + chain.name + '-' + str(residue.seqid.num)
-    return lig
+    return lig, str(distance)
 
 
-def items_to_check():
-    number_of_modeled_structures = 0
-    number_of_structures_with_at_least_one_high_confidence_ligand = 0
-    number_of_modeled_ligands = 0
-    number_of_high_confidence_ligands = 0
-    number_of_low_confidence_ligands = 0
+#def items_to_check():
+#    number_of_modeled_structures = 0
+#    number_of_structures_with_at_least_one_high_confidence_ligand = 0
+#    number_of_modeled_ligands = 0
+#    number_of_high_confidence_ligands = 0
+#    number_of_low_confidence_ligands = 0
 
 
-def export_pandda_models(panddaDir):
+def run_giant_merge_conformations(sample_id):
+    cmd = (
+        'giant.merge_conformations '
+        ' major={0!s}-pandda-input.pdb '.format(sample_id) +
+        ' minor=modelled_structures/{0!s}-pandda-model.pdb '.format(sample_id) +
+        ' > /dev/null'
+    )
+    print(cmd)
+
+def prepare_model(panddaDir, sample_id, ensembleOnly, overwrite):
+    print("--> ensemble model preparation")
+    if ensembleOnly:
+        os.chdir(os.path.join(panddaDir, 'processed_datasets', sample_id))
+        if not os.path.isfile("multi-state-model.pdb"):
+            run_giant_merge_conformations(sample_id)
+        elif os.path.isfile("multi-state-model.pdb") and overwrite:
+            run_giant_merge_conformations(sample_id)
+        else:
+            print('Warning: file exists (select -o option if you want to overwrite); skipping...')
+    else:
+        print("default single conformer model remains active "
+              "(choose option -n if you want to export an ensemble mode; skipping...")
+
+def files_to_delete():
+    files = [
+        'multi-state-model.pdb',
+        'multi-state-restraints.refmac.params',
+        'multi-state-restraints.phenix.params',
+        'ground-state-average-map.native.mtz',
+        'z_map.native.mtz',
+        'pandda-model.pdb'
+             ]
+    return files
+
+def prepare_destination_dir(destinationDir, sample_id, overwrite):
+    print("--> destination directory preparation")
+    print("checking if sample folder exisits in {0!s}".format(destinationDir))
+    os.chdir(destinationDir)
+    if os.path.isdir(sample_id):
+        print('sample directory exists; skipping...')
+    else:
+        print("creating sample directory")
+        os.mkdir(sample_id)
+    os.chdir(sample_id)
+    if overwrite:
+        for f in glob.glob('event_*.native.mtz'):
+            print("removing " + f)
+            os.remove(f)
+        for f in files_to_delete():
+            os.remove(f)
+            print("removing " + f)
+
+
+def linking_files_to_destination_dir(destinationDir, sample_id, panddaDir, ensembleOnly, model):
+    print("--> linking files to destination directory")
+    os.chdir(os.path.join(destinationDir, sample_id))
+    if ensembleOnly:
+        if os.path.isfile(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-model.pdb')):
+            print('ln -s {0!s}'.format(os.path.relpath(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-model.pdb'))))
+        if os.path.isfile(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-restraints.refmac.params')):
+            print('ln -s {0!s}'.format(os.path.relpath(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-restraints.refmac.params'))))
+        if os.path.isfile(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-restraints.phenix.params')):
+            print('ln -s {0!s}'.format(os.path.relpath(os.path.join(panddaDir, 'processed_datasets', sample_id, 'multi-state-restraints.phenix.params'))))
+    else:
+        if os.path.isfile(model):
+            print('ln -s {0!s} pandda-model.pdb'.format(os.path.relpath(model)))
+    if os.path.isfile(os.path.join(panddaDir, 'processed_datasets', sample_id, "{0!s}-ground-state-average-map.native.mtz".format(sample_id))):
+            print('ln -s {0!s} ground-state-average-map.native.mtz'.format(os.path.relpath(os.path.join(panddaDir, 'processed_datasets', sample_id, "{0!s}-ground-state-average-map.native.mtz".format(sample_id)))))
+    if os.path.isfile(os.path.join(panddaDir, 'processed_datasets', sample_id, "{0!s}-z_map.native.mtz".format(sample_id))):
+            print('ln -s {0!s} z_map.native.mtz'.format(os.path.relpath(os.path.join(panddaDir, 'processed_datasets', sample_id, "{0!s}-z_map.native.mtz".format(sample_id)))))
+    for event in glob.glob(os.path.join(panddaDir, 'processed_datasets', sample_id, "{0!s}-event_*_map.native.mtz".format(sample_id))):
+        filename = event[event.rfind('/')+1:]
+        new_filename = filename.replace(sample_id + '-', '')
+        print('ln -s {0!s} {1!s}'.format(event, new_filename))
+
+def export_pandda_models(panddaDir, destinationDir, export, highconfidenceOnly, lowconfidenceOnly, ensembleOnly, overwrite):
     inspect_csv = read_inspect_event_csv_as_list(panddaDir)
     ligand_confidence_index = get_ligand_confidence_index(inspect_csv)
     for str in sorted(glob.glob(os.path.join(panddaDir, 'processed_datasets', '*',
                                               'modelled_structures','*-pandda-model.pdb'))):
         sample_id = str.split('/')[len(str.split('/'))-3]
         print('{0!s}:\n'.format(sample_id))
-        get_info(inspect_csv, sample_id, ligand_confidence_index, str)
+        ligand_confidence_list = get_info(inspect_csv, sample_id, ligand_confidence_index, str)
+
+        if export:
+            if highconfidenceOnly:
+                if "high confidence" in ligand_confidence_list:
+                    prepare_model(panddaDir, sample_id, ensembleOnly, overwrite)
+                    prepare_destination_dir(destinationDir, sample_id, overwrite)
+                    linking_files_to_destination_dir(destinationDir, sample_id, panddaDir, ensembleOnly, model)
+            elif lowconfidenceOnly:
+                if "low confidence" in ligand_confidence_list and not "high confidence" in ligand_confidence_list:
+                    prepare_model(panddaDir, sample_id, ensembleOnly, overwrite)
 
 
 
-#        if sample_id not in sampleList:
-#            print('converting maps for {0!s}'.format(sample_id))
-#            sampleList.append(sample_id)
-#        workDir = maps[:maps.rfind('/')]
-#        os.chdir(workDir)
-#        pandda_input_mtz = '{0!s}-pandda-input.mtz'.format(sample_id)
-#        if os.path.isfile(pandda_input_mtz):
-#            dmin = get_resolution(pandda_input_mtz)
-#        else:
-#            print('ERROR: {0!s} does not exist; cannot get resolution of map later; skipping...'.format(pandda_input_mtz))
-#            sys.exit(2)
-#        map_name = maps.split('/')[len(maps.split('/'))-1]
-#        print('current map {0!s}'.format(map_name))
-#        mtz_name = map_name.replace('.ccp4', '.mtz')
-#        if os.path.isfile(mtz_name) and not overwrite:
-#            print('WARNING: {0!s} exists; skipping...'.format(mtz_name))
-#            continue
-#        if axisOrder:
-#            tmp_map_name = map_name.replace('.ccp4', '_tmp.ccp4')
-#            map_name = change_axis_order(map_name, tmp_map_name, axisOrder)
-#        run_gemmi_aap2sf(map_name, mtz_name, dmin)
-#        if tmp_map_name:
-#            remove_temp_map(tmp_map_name)
+
+
 
 
 def usage():
     usage = (
         '\n'
-        'convert_event_map_to_mtz.py converts pandda event, z and average maps to mtz format\n'
+        'export_pandda_models.py exports structures modelled with pandda.inspect or inspect_pandda_analyse.py\n'
+        'into a new destination directory. The script will create subfolders corresponding to the exported \n'
+        "datasets if they don't exisit in the destination directory. Otherwise, it will just copy the files \n"
+        "into the respective folders.\n"
         '\n'
         'usage:\n'
-        'ccp4-python convert_event_map_to_mtz.py -p <pandda_dir>\n'
+        'ccp4-python export_pandda_models.py -p <pandda_dir>\n'
         'e.g.\n'
-        'ccp4-python convert_event_map_to_mtz.py -p /data/user/pandda\n'
+        'ccp4-python export_pandda_models.py -p /data/user/pandda\n'
         'Default: export all structures that have a ligand modelled as ensemble model'
         '\n'
         'additional command line options:\n'
-        '--highconfidence, -a AXIS_ORDER\n'
-        '    changes axis order of input map as specified, e.g. -a zyx\n'
+        '--export, -e\n'
+        '    Export models (default is to only analyse models in pandda folder).\n'
+        '--destination, -d\n'
+        '    Directory where models should be exported to.\n'
+        '--highconfidence, -c\n'
+        '    Export only high confidence hits (as labelled in pandda_inspect_events.csv).\n'
+        '--lowconfidence, -l\n'
+        '    Export only low confidence hits (as labelled in pandda_inspect_events.csv).\n'
+        '--ensemble, -n\n'
+        '    Export models as ensample models (default is as single conformer models).\n'
         '--overwrite, -o\n'
-        '    flag to overwrite existing mtz files\n'
+        '    Flag to overwrite previously exported files.\n'
     )
     print(usage)
 
 def main(argv):
     panddaDir = None
+    destinationDir = None
     export = False
     highconfidenceOnly = False
     lowconfidenceOnly = False
     ensembleOnly = True
-    singleOnly = False
-    mixed = False
+    overwrite = False
 
     try:
-        opts, args = getopt.getopt(argv,"p:ha",["panddadir=", "export"])
+        opts, args = getopt.getopt(argv,"p:d:henclo",["panddadir=", "destinationdir=", "export", "ensemble",
+                                                   "highconfidence", "lowconfidence", "overwrite"])
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -156,16 +239,23 @@ def main(argv):
             sys.exit()
         elif opt in ("-p", "--panddadir"):
             panddaDir = arg
+        elif opt in ("-d", "--destinationdir"):
+            destinationDir = arg
         elif opt in ("-e", "--export"):
             export = True
-        elif opt in ("-hi", "--highconfidence"):
+        elif opt in ("-n", "--ensemble"):
+            ensembleOnly = True
+        elif opt in ("-c", "--highconfidence"):
             highconfidenceOnly = True
-        elif opt in ("-lo", "--lowconfidence"):
+        elif opt in ("-l", "--lowconfidence"):
             lowconfidenceOnly = True
+        elif opt in ("-o", "--overwrite"):
+            overwrite = True
 
 
     if os.path.isdir(panddaDir):
-        export_pandda_models(panddaDir)
+        export_pandda_models(panddaDir, destinationDir, export, highconfidenceOnly, lowconfidenceOnly,
+                             ensembleOnly, overwrite)
     else:
         print('ERROR: pandda directory does not exist -> {0!s}'.format(panddaDir))
 
